d995f39c0a1932993f80fd240ee4c53c0682eaff
Alex Popescu <pop.alx@gmail.com>
2024-02-01 20:04:47 +0200

    Azure AD Auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from msal_streamlit_authentication import msal_authentication
 
 """
 # Welcome to Streamlit!
@@ -13,28 +14,53 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+login_token = msal_authentication(
+    auth={
+        "clientId": "aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee",
+        "authority": "https://login.microsoftonline.com/bbbbbbbb-cccc-dddd-eeee-fffffffffff",
+        "redirectUri": "/",
+        "postLogoutRedirectUri": "/"
+    }, # Corresponds to the 'auth' configuration for an MSAL Instance
+    cache={
+        "cacheLocation": "sessionStorage",
+        "storeAuthStateInCookie": False
+    }, # Corresponds to the 'cache' configuration for an MSAL Instance
+    login_request={
+        "scopes": ["aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee/.default"]
+    }, # Optional
+    logout_request={}, # Optional
+    login_button_text="Login", # Optional, defaults to "Login"
+    logout_button_text="Logout", # Optional, defaults to "Logout"
+    class_name="css_button_class_selector", # Optional, defaults to None. Corresponds to HTML class.
+    html_id="html_id_for_button", # Optional, defaults to None. Corresponds to HTML id.
+    key=1 # Optional if only a single instance is needed
+)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+if login_token is not None:
+    st.write("Recevied login token:", login_token)
+    st.write("You are now logged in !")
+    num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+    num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    indices = np.linspace(0, 1, num_points)
+    theta = 2 * np.pi * num_turns * indices
+    radius = indices
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    x = radius * np.cos(theta)
+    y = radius * np.sin(theta)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    df = pd.DataFrame({
+        "x": x,
+        "y": y,
+        "idx": indices,
+        "rand": np.random.randn(num_points),
+    })
+
+    st.altair_chart(alt.Chart(df, height=700, width=700)
+        .mark_point(filled=True)
+        .encode(
+            x=alt.X("x", axis=None),
+            y=alt.Y("y", axis=None),
+            color=alt.Color("idx", legend=None, scale=alt.Scale()),
+            size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+        ))
